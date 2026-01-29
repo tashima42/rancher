5c73f23ad5a66bce95a9dfcf113983e6628776f7
Pedro Tashima <tithe.coup0l@icloud.com>
2026-01-29 09:39:41 -0300

    update components images files script

--- scripts/create-components-images-files.sh
+++ scripts/create-components-images-files.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
- set -e
+set -ex
 
 cd $(dirname $0)
 
@@ -10,7 +10,7 @@ source package-env
 cd ../package
 
 if [ ${ARCH} == arm64 ]; then
-    ETCD_UNSUPPORTED_ARCH=arm64
+  ETCD_UNSUPPORTED_ARCH=arm64
 fi
 
 mkdir -p ../dist
@@ -18,19 +18,22 @@ mkdir -p ../dist
 cd ../bin
 
 if [ ! -d $CHART_REPO_DIR ]; then
-    git clone --branch $CHART_DEFAULT_BRANCH https://github.com/rancher/charts $CHART_REPO_DIR
+  git clone --branch $CHART_DEFAULT_BRANCH https://github.com/rancher/charts $CHART_REPO_DIR
 fi
 
 CHARTS_DIRS="${CHARTS_DIRS:+${CHARTS_DIRS},}${CHART_REPO_DIR}"
 
 if [ ! -d $SMALL_FORK_REPO_DIR ]; then
-    mkdir -p $SMALL_FORK_REPO_DIR
-    git clone --branch main https://github.com/rancher/charts-small-fork $SMALL_FORK_REPO_DIR
+  mkdir -p $SMALL_FORK_REPO_DIR
+  git clone --branch main https://github.com/rancher/charts-small-fork $SMALL_FORK_REPO_DIR
 fi
 
 if [ ${ARCH} == amd64 ]; then
-    # Move this out of ARCH check for local dev on non-amd64 hardware.
-    TAG=$TAG REPO=${REPO} go run ../pkg/image/export/main.go $CHARTS_DIRS $IMAGE $AGENT_IMAGE $SYSTEM_AGENT_UPGRADE_IMAGE $WINS_AGENT_UPGRADE_IMAGE $CLUSTER_API_CONTROLLER_IMAGE ${SYSTEM_AGENT_INSTALLER_RKE2_IMAGES[@]} ${SYSTEM_AGENT_INSTALLER_K3S_IMAGES[@]}
+  # Move this out of ARCH check for local dev on non-amd64 hardware.
+  go run ../cmd/images/main.go \
+    --tag "$TAG" \
+    --charts-path "$CHARTS_DIRS" \
+    --extra-images "$IMAGE,$AGENT_IMAGE,$SYSTEM_AGENT_UPGRADE_IMAGE,$WINS_AGENT_UPGRADE_IMAGE,$CLUSTER_API_CONTROLLER_IMAGE,$(printf "%s," "${SYSTEM_AGENT_INSTALLER_RKE2_IMAGES[@]}" | sed 's/,$//'),$(printf "%s," "${SYSTEM_AGENT_INSTALLER_K3S_IMAGES[@]}" | sed 's/,$//')"
 fi
 
 # Create components file used for pre-release notes
